d92dd0ce5e4ddd3f6839a243282fe994b209aa0b
christophe le drean <christophe@rouge-rose.net>
2012-09-05 11:37:19 +0200

    premier commit

---
+++ squelettes/lang/local_en.php
@@ -0,0 +1,25 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+   // C
+   'contact' => 'Contact',
+   'credits_design' => "Design & development <a href='@urldesign@'>christophe le drean</a> with <a href='@urlspip@'>spip</a>",
+
+   //M
+   'signatories_coalition' => "Signatories",
+   'signatories' => 'signatories',
+
+   // R
+   'sign_on' => 'Sign on the Washington Call',
+
+   // S
+   'suite' => 'Read more',
+   'suivre_facebook' => "Follow us on <a href='@urlfacebook'>Facebook</a>",
+
+   // U
+   'updated' => 'Updated'
+
+);
+?>
---
+++ squelettes/lang/local_fr.php
@@ -0,0 +1,24 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+   // C
+   'contact' => 'Contact',
+   'credits_design' => "Design & développement <a href='@urldesign@'>christophe le drean</a> avec <a href='@urlspip@'>spip</a>",
+
+   //M
+   'signatories_coalition' => "Les signataires",
+   'signatories' => 'signataires',
+
+   // R
+   'sign_on' => "Signer l'appel de Washington",
+
+   // S
+   'suite' => 'Lire la suite',
+   'suivre_facebook' => "Suivez-nous sur <a href='@urlfacebook'>Facebook</a>",
+
+   // U
+   'updated' => 'Mise à jour le'
+);
+?>
