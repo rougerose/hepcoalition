c61b84ba3db8a059bf8bf347c4e4cdc793c566fd
christophe le drean <christophe@rouge-rose.net>
2012-09-07 10:47:41 +0200

    page signataires et langues : - page signataires : présentation en tableau - langues : ajouts

--- squelettes/lang/local_en.php
+++ squelettes/lang/local_en.php
@@ -7,14 +7,24 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
    'contact' => 'Contact',
    'credits_design' => "Design & development <a href='@urldesign@'>christophe le drean</a> with <a href='@urlspip@'>spip</a>",
 
+   // E
+   'email' => 'e-mail',
+
    //M
    'signatories_coalition' => "Signatories",
    'signatories' => 'signatories',
+
+   // O
+   'organisation' => 'Organization',
+
+   // P
+   'pays' => 'Country',
 
    // R
    'sign_on' => 'Sign on the Washington Call',
 
    // S
+   'siteweb' => 'Web site',
    'suite' => 'Read more',
    'suivre_facebook' => "Follow us on <a href='@urlfacebook'>Facebook</a>",
 
--- squelettes/lang/local_fr.php
+++ squelettes/lang/local_fr.php
@@ -7,14 +7,24 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
    'contact' => 'Contact',
    'credits_design' => "Design & développement <a href='@urldesign@'>christophe le drean</a> avec <a href='@urlspip@'>spip</a>",
 
+   // E
+   'email' => 'e-mail',
+
    //M
    'signatories_coalition' => "Les signataires",
    'signatories' => 'signataires',
+
+   // O
+   'organisation' => 'Organisation',
+
+   // P
+   'pays' => 'Pays',
 
    // R
    'sign_on' => "Signez l'appel de Washington",
 
    // S
+   'siteweb' => 'Site web',
    'suite' => 'Lire la suite',
    'suivre_facebook' => "Suivez-nous sur <a href='@urlfacebook'>Facebook</a>",
 
