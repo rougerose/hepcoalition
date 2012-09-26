a1e014b4c82ff5ed18b4b9414a1d2837d5273247
christophe le drean <christophe@rouge-rose.net>
2012-09-26 19:57:03 +0200

    ajouts langues : - russe - espagnol - arabe

---
+++ squelettes/lang/local_es.php
@@ -0,0 +1,37 @@
+<?php
+
+// espagnol
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+   // C
+   'contact' => 'Contacto',
+   'credits_design' => "Diseño y desarrollo <a href='@urldesign@'>christophe le drean</a> con <a href='@urlspip@'>SPIP</a>",
+
+   // E
+   'email' => 'correo electrónico',
+
+   //M
+   'signatories_coalition' => "Signatarios",
+   'signatories' => 'signatarios',
+
+   // O
+   'organisation' => 'organización',
+
+   // P
+   'pays' => 'país',
+
+   // R
+   'sign_on' => 'Firmar el llamamiento de Washington',
+
+   // S
+   'siteweb' => 'Página web',
+   'suite' => 'Leer más',
+   'suivre_facebook' => "Síganos en <a href='@urlfacebook'>Facebook</a>",
+
+   // U
+   'updated' => 'Updated'
+
+);
+?>
---
+++ squelettes/lang/local_ru.php
@@ -0,0 +1,35 @@
+<?php
+// russe
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+   // C
+   'contact' => 'Контактная информация',
+   'credits_design' => "Дизайн и разработка: <a href='@urldesign@'>Кристоф ле Дреан</a> при использовании системы управления контентом <a href='@urlspip@'>SPIP</a>",
+
+   // E
+   'email' => 'эл. почта',
+
+   //M
+   'signatories_coalition' => "подписанты",
+   'signatories' => 'подписанты',
+
+   // O
+   'organisation' => 'организация',
+
+   // P
+   'pays' => 'страна',
+
+   // R
+   'sign_on' => 'Подпишите обращение Вашингтона',
+
+   // S
+   'siteweb' => 'веб-сайт',
+   'suite' => 'Читать дальше',
+   'suivre_facebook' => "Читать о нас на <a href='@urlfacebook'>Facebook</a>",
+
+   // U
+   'updated' => 'Updated'
+
+);
+?>
