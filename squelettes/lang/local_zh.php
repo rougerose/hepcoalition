9a339fea801b92bfd3f0ad4f9243db90378ff0eb
christophe le drean <christophe@rouge-rose.net>
2012-11-20 17:00:55 +0100

    fichier traduction chinois (à compléter)

---
+++ squelettes/lang/local_zh.php
@@ -0,0 +1,35 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+   // C
+   'contact' => 'Contact',
+   'credits_design' => "Design & development <a href='@urldesign@'>christophe le drean</a> with <a href='@urlspip@'>spip</a>",
+
+   // E
+   'email' => 'e-mail',
+
+   //M
+   'signatories_coalition' => "&#31614;&#21517;",
+   'signatories' => 'signatories',
+
+   // O
+   'organisation' => 'Organization',
+
+   // P
+   'pays' => 'Country',
+
+   // R
+   'sign_on' => 'Sign on the Washington Call',
+
+   // S
+   'siteweb' => 'Web site',
+   'suite' => 'Read more',
+   'suivre_facebook' => "Follow us on <a href='@urlfacebook'>Facebook</a>",
+
+   // U
+   'updated' => 'Updated'
+
+);
+?>
