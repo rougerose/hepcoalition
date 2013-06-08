54adfe36dcacf47ea8558ad0341bf77ae1b97cfa
christophe le drean <christophe@rouge-rose.net>
2013-06-08 16:30:06 +0200

    suite : - composition article + logo grand format - petition avec Formidable - pied de page à compléter

--- hepcoalition_fonctions.php
+++ hepcoalition_fonctions.php
@@ -2,4 +2,9 @@
 
 // $GLOBALS['z_blocs'] = array('content','extra1','extra2','head','head_js','foot_js','header','footer');
 
+function tri_alpha($array) {
+      natcasesort($array);
+      return $array;
+   }
+
 ?>
--- lang/local_en.php
+++ lang/local_en.php
@@ -16,6 +16,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
    //M
    'more' => 'More',
 
+   //N
+   'name_first' => 'First name',
+   'name_last' => 'Last name',
+
    // O
    'organisation' => 'Organization',
 
@@ -27,7 +31,12 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
    // S
    'signatories' => 'signatories',
    'signatories_coalition' => "Signatories",
+   'signatories_singular' => "1 supporter",
+   'signatories_plural' => "@nb@ supporters",
+   'signatories_plural_with' => 'With @nb@ supporters',
+   'signatories_singular_with' => 'With 1 supporter',
    'sign_on' => 'Sign on the Washington Call',
+   'sign_petition' => 'Sign the petition',
    'siteweb' => 'Web site',
    'suite' => 'Read more',
    'suivre_facebook' => "Follow us on <a href='@urlfacebook@'>Facebook</a>",
