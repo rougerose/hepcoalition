d7ecc547a35f558f48d4aad10606eb026a8d0337
christophe le drean <christophe@rouge-rose.net>
2013-06-26 10:38:34 +0200

    début carousel

--- js/hepcoalition.js
+++ js/hepcoalition.js
@@ -2,4 +2,21 @@ jQuery(document).ready(function($) {
     $(".menu-lang--langue").click(function(){
         $("#menu_lang").toggleClass("active");
     });
+
+    // flexslider
+    $('.flexslider').flexslider({
+         animation: "slide",
+         slideshow: false,
+         start: function(slider){
+           slider.viewport.css({"overflow":"visible"});
+           slider.newSlides.css({'opacity':0.3});
+           slider.slides.eq(slider.currentSlide).fadeTo('slow',1);
+         },
+         before: function(slider) {
+            slider.slides.fadeTo('slow',0.3);
+         },
+         after: function(slider) {
+            slider.slides.eq(slider.currentSlide).fadeTo('slow',1);
+         }
+    });
 });
