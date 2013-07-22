8958509ff379fec83de56ef97a9ae95c41863533
christophe le drean <christophe@rouge-rose.net>
2013-07-22 14:53:39 +0200

    article défaut

--- hepcoalition_options.php
+++ hepcoalition_options.php
@@ -2,4 +2,7 @@
 
 $GLOBALS['z_blocs'] = array('content','extra1','extra2','head','head_js','foot_js','header','footer');
 
+$GLOBALS['debut_intertitre'] = "\n<h2 class='spip'>";
+$GLOBALS['fin_intertitre'] = "</h2>\n";
+
 ?>
