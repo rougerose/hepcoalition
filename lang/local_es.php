04f792ceb9a5754fc7ded030676329ea4742ed94
christophe le drean <christophe@rouge-rose.net>
2013-07-26 10:42:25 +0200

    mise à jour espagnol

--- lang/local_es.php
+++ lang/local_es.php
@@ -3,83 +3,84 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-   // A
-   'agenda'                         => 'Calendario',
-   'about_us'                       => 'Quiénes somos',
-   
-   // C
-   'campaigns_all'                  => 'All campaigns',
-   'campaign_last'                  => 'Last campaign',
-   'contact'                        => 'Contacto',
-   'credits_design'                 => "Design & développement <a href='@urldesign@'>christophe le drean</a> avec <a href='@urlspip@'>spip</a>",
-   
-   // E
-   'email'                          => 'e-mail',
-   'email_send'                     => "Send us an <a href='@urlcontact@' class='spip_out'>email</a>",
-   
-   // F
-   'follow_us'                      => "Follow us", // on Facebook
-   
-   // H
-   'home'                           => 'Inicio',
-   
-   // I
-   'info_recherche_article'         => '1 article',
-   'info_recherche_articles'        => '@nb@ articles',
-   'info_recherche_document'        => '1 file',
-   'info_recherche_documents'       => '@nb@ files',
-   'info_recherche_label'           => 'Search',
-   'info_recherche_mot'             => '1 keyword',
-   'info_recherche_mots'            => '@nb@ keywords',
-   'info_recherche_rubrique'        => '1 section',
-   'info_recherche_rubriques'       => '@nb@ sections',
-   'info_recherche_site'            => '1 web site',
-   'info_recherche_sites'           => '@nb@ web sites',
-   'info_recherche_resultat'        => '1 result',
-   'info_recherche_resultats'       => '@nb@ results',
-   'info_recherche_pas_de_resultat' => 'No result',
-   
-   // L
-   'lastest_news'                   => "Lastest news",
-   'latest_publications'            => 'Latest publications',
-   
-   //M
-   'more'                           => 'Leer más',
-   
-   //N
-   'name_first'                     => 'Nombre',
-   'name_last'                      => 'Apellidos',
-   'news_last_publications'         => 'Noticias y últimas publicaciones',
-   'no_event_available'             => 'no event available',
-   
-   // O
-   'organisation'                   => 'organización',
-   
-   // P
-   'page'                           => 'página',
-   'pays'                           => 'País',
-   
-   // R
-   
-   // S
-   'share'                          => 'Share',
-   'signatories'                    => 'firmantes',
-   'signatories_coalition'          => "Firmantes",
-   'signatories_singular'           => "1 apoyo",
-   'signatories_plural'             => "@nb@ apoyos",
-   'signatories_plural_with'        => 'Con @nb@ apoyos',
-   'signatories_singular_with'      => 'Con 1 apoyo',
-   'sign_on'                        => 'Firmar',
-   'sign_on_wc'                     => "Firmar el llamamiento de Washington",
-   'sign_petition'                  => 'Firma la petición',
-   'siteweb'                        => 'Página web',
-   'suite'                          => 'Leer más',
-   'suivre_facebook'                => "Síganos en <a href='@urlfacebook@'>Facebook</a>",
-   
-   // T
-   'tags'                           => 'Tags',
-   
-   // U
-   'updated'                        => 'Updated'
+      // A
+      'agenda'                         => 'Calendario'
+      'about_us'                       => 'Quiénes somos',
+      
+      // C
+      'campaigns_all'                  => 'Todas las campañas',
+      'campaign_last'                  => 'Última campaña',
+      'contact'                        => 'Contacto',
+      'credits_design'                 => "Diseño y desarrollo <a href='@urldesign@'>christophe le drean</a> with <a href='@urlspip@'>spip</a>",
+      
+      // E
+      'email'                          => 'e-mail',
+      'email_send'                     => "Envíenos un <a href='@urlcontact@' class='spip_out'>e-mail</a>",
+      
+      // F
+      'follow_us'                      => "Síganos", // en Facebook
+      
+      // H
+      'home'                           => 'Inicio',
+      
+      // I
+      'info_recherche_article'         => '1 artículo',
+      'info_recherche_articles'        => '@nb@ artículos',
+      'info_recherche_document'        => '1 archivo',
+      'info_recherche_documents'       => '@nb@ archivos',
+      'info_recherche_label'           => 'Buscar',
+      'info_recherche_mot'             => '1 palabra clave',
+      'info_recherche_mots'            => '@nb@ palabras claves',
+      'info_recherche_rubrique'        => '1 sección',
+      'info_recherche_rubriques'       => '@nb@ secciones',
+      'info_recherche_site'            => '1 página web',
+      'info_recherche_sites'           => '@nb@ páginas web',
+      'info_recherche_resultat'        => '1 resultado',
+      'info_recherche_resultats'       => '@nb@ resultados',
+      'info_recherche_pas_de_resultat' => 'Ningún resultado',
+      
+      // L
+      'lastest_news'                   => "Últimas noticias",
+      'latest_publications'            => 'Últimas publicaciones',
+      
+      //M
+      'more'                           => 'Leer más',
+      
+      //N
+      'name_first'                     => 'Nombre',
+      'name_last'                      => 'Apellidos',
+      'news_last_publications'         => 'Noticias y últimas publicaciones',
+      'no_event_available'             => 'Ningún evento disponible',
+      
+      // O
+      'organisation'                   => 'Organización',
+      
+      // P
+      'page'                           => 'página',
+      'pays'                           => 'País',
+      'press_release'                  => 'Comunicado de prensa',
+      
+      // R
+      
+      // S
+      'share'                          => 'Compartir',
+      'signatories'                    => 'firmantes',
+      'signatories_coalition'          => "Firmantes",
+      'signatories_singular'           => "1 apoyo",
+      'signatories_plural'             => "@nb@ apoyos",
+      'signatories_plural_with'        => 'Con@nb@ apoyos',
+      'signatories_singular_with'      => 'Con 1 apoyo',
+      'sign_on'                        => 'Firmar',
+      'sign_on_wc'                     => 'Firmar el llamamiento de Washington',
+      'sign_petition'                  => 'Firmar la petición',
+      'siteweb'                        => 'Página web',
+      'suite'                          => 'Leer más',
+      'suivre_facebook'                => "Síganos en <a href='@urlfacebook@'>Facebook</a>",
+      
+      // T
+      'tags'                           => 'Etiquetas',
+      
+      // U
+      'updated'                        => 'Actualizado'
 );
 ?>
