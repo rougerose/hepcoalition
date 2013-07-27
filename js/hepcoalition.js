4633ce1930ae3af81b76ce29b439710027b5207a
christophe le drean <christophe@rouge-rose.net>
2013-07-27 11:52:10 +0200

    formulaire recherche version touch

--- js/hepcoalition.js
+++ js/hepcoalition.js
@@ -3,9 +3,9 @@ jQuery(document).ready(function($) {
         $("#menu_lang").toggleClass("active");
     });
 
-    $(".touch #formulaire_recherche").click(function(){
+    /*$(".touch #formulaire_recherche").click(function(){
         $(this).find("> form").toggleClass("active");
-    });
+    });*/
 
     // flexslider
     $('.flexslider').flexslider({
@@ -24,4 +24,43 @@ jQuery(document).ready(function($) {
             slider.slides.eq(slider.currentSlide).fadeTo('slow',1);
          }
     });
+
+    $("form.search__form:not:(.open-search)").bind("touchstart",function(e){
+        e.preventDefault();
+        $(this).addClass("open-search").unbind("touchstart");
+        $("input[type=search]").css("font-size","16px");
+    });
+    $(".touch input[type=search]").blur(function(){
+        $("input[type=search]").css("font-size","10px");
+        $("form.main-search").removeClass("open-search");
+    });
+    $("form.search__form").submit(function(e){
+        if($("input[name=recherche]").val()===""){
+            $("input[name=recherche]").attr("placeholder","Please, type something").focus();
+            return false;
+        }
+        else {
+            return true;
+        }
+    });
+
+    /*$("form.main-search:not(.open-search)").bind("touchstart",function(e){
+        e.preventDefault();
+        $(this).addClass("open-search").unbind("touchstart");
+        $("input[type=search]").css("font-size","16px");
+    });
+    $(".touch input[type=search]").blur(function(){
+        $("input[type=search]").css("font-size","10px");
+        $("form.main-search").removeClass("open-search");
+    });
+    $("form.main-search").submit(function(e){
+        if($("input[name=keywords]").val()==""){
+            $("input[name=keywords]").attr("placeholder","Oh come on, search for something.").focus();
+            return false;
+        }
+        else{return true;}
+    });*/
+
+
+
 });
