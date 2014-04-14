281914b15026809499d562e78c7cf2ccc3c4bd73
christophe le drean <christophe@rouge-rose.net>
2014-04-14 21:58:52 +0200

    désactivé le js pour le menu en version touch qui était difficile à manipuler et comprendre.

--- js/hepcoalition.js
+++ js/hepcoalition.js
@@ -12,9 +12,6 @@ jQuery(document).ready(function($) {
             $(this).toggleClass("active");
             $np.toggleClass("open");
         });
-        // http://osvaldas.info/drop-down-navigation-responsive-and-touch-friendly
-        $hasUl.find("> a").wrapInner('<span />');
-        $hasUl.doubleTapToGo();
     });
 
     // flexslider
