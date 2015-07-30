bd3c413f738bef40e45a0ea35f09dc5abb150a7e
christophe <christophe@rouge-rose.net>
2015-07-30 17:06:53 +0200

    traductions: - le mot en français n'était pas traduit. - corrections de mise en forme.

--- lang/local_fr.php
+++ lang/local_fr.php
@@ -6,25 +6,25 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
    // A
    'agenda'                         => 'Agenda',
    'about_us'                       => "À propos",
-   
+
    // B
    'by'                             => 'Par',
-   
+
    // C
    'campaigns_all'                  => 'Toutes les campagnes',
    'campaign_last'                  => 'Dernière campagne',
    'contact'                        => 'Contact',
    'credits_design'                 => "Design & développement <a href='@urldesign@'>christophe le drean</a> avec <a href='@urlspip@'>spip</a>",
-   
+
    // E
    'email'                          => 'e-mail',
    'email_send'                     => "Nous envoyer un <a href='@urlcontact@' class='spip_out'>email</a>",
    // F
    'follow_us'                      => "Suivez-nous", // on Facebook
-   
+
    // H
-   'home'                           => 'Accueil', 
-   
+   'home'                           => 'Accueil',
+
    // I
    'info_recherche_article'         => '1 article',
    'info_recherche_articles'        => '@nb@ articles',
@@ -40,32 +40,32 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
    'info_recherche_resultat'        => '1 résultat',
    'info_recherche_resultats'       => '@nb@ résultats',
    'info_recherche_pas_de_resultat' => 'Aucun résultat',
-   
+
    // L
    'lastest_news'                   => "Dernières actualités",
    'latest_publications'            => 'Dernières publications',
-   
+
    //M
    'message_404_title'              => "Page indisponible.",
    'message_404_desc'               => "Nous sommes désolé-e-s, la page que vous recherchez n'est pas disponible.<br />Revenir à <a href='@urlsite@' class='spip_out'>la page d'accueil du site</a>.",
    'more'                           => 'La suite',
-   
+
    //N
    'name_first'                     => 'Prénom',
    'name_last'                      => 'Nom',
    'navigation'                     => 'Navigation',
    'news_last_publications'         => 'Actualités & dernières publications',
    'no_event_available'             => 'Aucun événement disponible',
-   
+
    // O
    'organisation'                   => 'Organisation',
-   
+
    // P
    'page'                           => 'page',
    'pays'                           => 'Pays',
-   
+
    // R
-   
+
    // S
    'share'                          => 'Partager',
    'signatories_coalition'          => "Signataires",
@@ -77,9 +77,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
    'sign_on_wc'                     => "Signez l'appel de Washington",
    'sign_petition'                  => 'Signez la pétition',
    'siteweb'                        => 'Site web',
-   
+
    // T
    'tags'                           => 'Mots-clés',
-   'translation'                    => 'Traductions'
+   'translations'                    => 'Traductions'
 );
 ?>
