98c31158493d4e71d198388324674c4043ada2fd
christophe le drean <christophe@rouge-rose.net>
2021-06-11 19:19:11 +0200

    Menu principal, version smartphone

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 import Overlay from "@accede-web/overlay";
 
+/**
+ * Gestion des fenetres modales (y compris menu principal, version smartphone)
+ */
 const overlays = document.querySelectorAll("[data-open-modal-id]");
 
 if (overlays.length >= 1) {
@@ -9,13 +12,24 @@ if (overlays.length >= 1) {
     let btn = el;
     let selector = btn.getAttribute("data-open-modal-id");
     let modal = document.getElementById(selector);
+    let className = "overlay is-closed";
+    let closeSelector = ".modal_btnClose";
+    let closeBtn = "";
+    let sitenav = "";
+
+    if (selector === "modalMenu") {
+      className = "overlay overlay--full is-closed";
+      closeSelector = ".sitenav_closeMenu";
+      closeBtn = modal.querySelector("button");
+      sitenav = modal.querySelector(".sitenavOverlay");
+    }
+
     let overlay = new Overlay({
       content: modal,
       opener: btn,
-      className: "overlay is-closed",
+      className: className,
       role: "dialog",
-      titleSelector: ".modal_title",
-      closeSelector: ".modal_btnClose",
+      closeSelector: closeSelector,
       modal: true,
     });
 
@@ -25,23 +39,55 @@ if (overlays.length >= 1) {
       overlay.show();
       window.requestAnimationFrame(() => {
         overlay.el.classList.remove("is-closed");
+        if (closeBtn) {
+          closeBtn.classList.add("is-active");
+        }
+        if (sitenav) {
+          sitenav.classList.add("is-visible");
+        }
       });
     });
 
     overlay.addEventListener("close", () => {
       overlay.el.style.display = "flex";
+      sitenav.classList.remove("is-visible");
       overlay.el.addEventListener("transitionend", handleModalTransition);
       overlay.el.classList.add("is-closed");
       body.classList.remove("has-overlay");
       btn.classList.remove("is-active");
+
+      if (closeBtn) {
+        closeBtn.classList.remove("is-active");
+      }
     });
+
   });
 
   function handleModalTransition(event) {
     let overlay = event.target.closest("[aria-hidden]");
-    if (event.propertyName === "transform") {
+    // test cible ".modal" et transition "transform" ou "max-height"
+    if (event.target.classList[0] === "modal" && event.propertyName !== "opacity") {
       overlay.removeEventListener("transitionend", handleModalTransition);
       overlay.removeAttribute("style");
     }
   }
 }
+
+/**
+ * Animation du menu principal (version smartphone)
+ *
+ * code à supprimer
+ *
+ */
+// const modalMenu = document.getElementById("modalMenu");
+
+// if (modalMenu) {
+//   Array.prototype.forEach.call(
+//     modalMenu.querySelectorAll(
+//       ".sitenavOverlay_list > li.sitenavOverlay_item--section"
+//     ), function (item, index) {
+//       console.log(item, index);
+//       item.style.transitionDelay = (index + 2) / 10 + "s";
+//     }
+//   );
+// }
